986eb23d9e48e09106cb0aca1788f7ebcc0b2fc9
ToxaKaz <anton.a.kazancev@gmail.com>
2023-03-15 23:07:12 +0300

    Task_1 done

---
+++ tests/test_bfs.py
@@ -0,0 +1,31 @@
+import pytest
+from project import bfs
+from tests.utils import matrix_from_edges
+
+
+@pytest.mark.parametrize(
+    "size, edges, start, expected_ans",
+    [
+        (
+            4,
+            [(0, 1), (1, 2), (2, 3)],
+            0,
+            [0, 1, 2, 3],
+        ),
+        (3, [(0, 1), (1, 2), (2, 0)], 0, [0, 1, 2]),
+        (
+            6,
+            [(0, 1), (0, 2), (1, 4), (2, 3), (2, 5), (3, 2), (5, 3)],
+            0,
+            [0, 1, 1, 2, 2, 2],
+        ),
+        (
+            6,
+            [(0, 1), (0, 2), (1, 4), (2, 3), (2, 5), (3, 2), (5, 3)],
+            3,
+            [-1, -1, 1, 0, -1, 2],
+        ),
+    ],
+)
+def test_bfs(size, edges, start, expected_ans):
+    assert bfs(matrix_from_edges(size, edges), start) == expected_ans
