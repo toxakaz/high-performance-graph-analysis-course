6812614935ae5dbc58a336413cd8b96b5d8da52d
ToxaKaz <anton.a.kazancev@gmail.com>
2023-03-22 22:25:12 +0300

    utils file added in project. read from file func here.

---
+++ project/utils.py
@@ -0,0 +1,80 @@
+import pygraphblas as gb
+import networkx as nx
+from typing import List, Tuple
+
+
+__all__ = [
+    "matrix_from_edges",
+    "read_graph",
+    "is_NxN_bool_matrix",
+    "start_vertex_out_of_range_error",
+]
+
+
+def matrix_from_edges(
+    size: int, edges: List[Tuple[int, int]], is_directed: bool = True
+) -> gb.Matrix:
+    """Generates gb.Matrix from list of edges.
+
+    Args:
+        size (int): graph size
+        edges (List[Tuple[int, int]]): list of graph edges
+        is_directed (bool): True if graph is directed
+
+    Returns:
+        gb.Matrix: graph
+    """
+
+    graph = gb.Matrix.sparse(gb.types.BOOL, nrows=size, ncols=size)
+    for source, target in edges:
+        graph[source, target] = True
+        if not is_directed:
+            graph[target, source] = True
+
+    return graph
+
+
+def read_graph(path: str) -> gb.Matrix:
+    """Read graph from file.
+
+    Args:
+        path (str): path to graph
+
+    Returns:
+        gb.Matrix: graph
+    """
+
+    nxm = nx.nx_agraph.read_dot(path)
+
+    return matrix_from_edges(
+        nxm.number_of_nodes(),
+        [(int(source), int(target)) for source, target in nxm.edges()],
+    )
+
+
+def is_NxN_bool_matrix(graph: gb.Matrix):
+    """Checks the graph type and is graph matrix a square matrix.
+    Raises error if graph matrix is not NxN bool matrix.
+
+    Args:
+        graph (gb.Matrix): matrix of the graph
+
+    Returns:
+
+    """
+    if graph.type != gb.BOOL:
+        raise ValueError(f"Matrix type was {graph.type}, adjacency matrix expected")
+    if not graph.square:
+        raise ValueError("Adjacency matrix must be square")
+
+
+def start_vertex_out_of_range_error(vertex: int) -> ValueError:
+    """Creates vertex out of range error.
+
+    Args:
+        vertex (int): error vertex
+
+    Returns:
+        ValueError: error
+    """
+    return ValueError(f"Start vertex {vertex} is out of range")
